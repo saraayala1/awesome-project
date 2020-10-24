7e6559067753ae3c4db86ac2df77484212fad48d
saraayala1 <70584322+saraayala1@users.noreply.github.com>
2020-10-24 15:45:40 -0400

    added backgroud to venue

--- java/venue.js
+++ java/venue.js
@@ -1,9 +1,8 @@
 function insertComment(event){
-event.preventDefault();
 let username=document.querySelector("#username");
 let comment=document.querySelector("#comment");
-let output=document.querySelector("#output");
-output.innerHTML=`${username} says ${comment}`;
+alert(`Thank you ${username.value}!`);
+console.log(comment.value);
 }
 let submit=document.querySelector("#submit")
 submit.addEventListener("click", insertComment)
